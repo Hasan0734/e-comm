60dac7e76488a23886552520801b592958066038
Jahid Hasan <jahidhasan2856@gmail.com>
2022-08-13 17:03:49 +0600

    e-comm project start and comoplete header

---
+++ components/client/Header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import CartIcon from "../../Icons/CartIcon";
+import ProfileIcon from "../../Icons/ProfileIcon";
+import SearchIcon from "../../Icons/SearchIcon";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto px-5 py-5 header_area">
+      <div className="grid grid-cols-2">
+        <div className="flex gap-3">
+          <select>
+            <option>ENG</option>
+            <option>Ban</option>
+          </select>
+          <select>
+            <option>USD</option>
+            <option>EURO</option>
+          </select>
+        </div>
+        <div className="flex justify-end gap-6">
+          <button className="flex gap-2">
+            <ProfileIcon />
+            <span>My Profile</span>
+          </button>
+          <button className="flex gap-4 ">
+            <span className="relative">
+              <CartIcon />
+              <span className="items">2</span>
+            </span>
+
+            <span>Items</span>
+          </button>
+
+          <div className="flex gap-2">
+            <span className="opacity-50">$00.0</span>
+            <button>
+              {" "}
+              <SearchIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/client/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+import Navbar from "../Navbar/Navbar";
+
+export default function Layout() {
+  return (
+    <div>
+          <Header />
+          <Navbar/>
+    </div>
+  );
+}
